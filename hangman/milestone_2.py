7f54c721fdb7c6ee1986bf616a178ee473190f67
Moez Abdu <moezabdu@outlook.com>
2023-10-02 17:44:32 +0100

    refactor: Eliminates code duplication

--- hangman/milestone_2.py
+++ hangman/milestone_2.py
@@ -1,18 +1,15 @@
 import random
 
 # Creates list containing my 5 favourite fruits
-favourite_fruits = ["Apple", "Banana", "Mango", "Watermelon", "Pineapple"]
-# Assign this list to a variable called word_list and prints
-word_list = favourite_fruits
-print(word_list)
+word_list = ["Apple", "Banana", "Mango", "Watermelon", "Pineapple"]
 # Assign a randomly extracted word from word_list to the variable word and prints
 word = random.choice(word_list)
-print(word)
+print("The randomly chosen word is: ", word)
 
-# %% Takes user input for single letter
+# Takes user input for a single letter
 guess = input("Enter a single letter: ")
-if len(guess) == 1 and guess.lower() in "abcdefghijklmnopqrstuvwxyz":
+# Checks if input is a single character
+if len(guess) == 1 and guess.isalpha():
     print("Good guess!")
 else:
     print("Oops! That is not a valid input.")
-# %%
